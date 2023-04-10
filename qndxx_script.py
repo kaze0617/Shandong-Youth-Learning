1951edd945b3b48694f68d44c0a8e2cd4cf1fb85
KAZE <1515097306@qq.com>
2023-04-10 23:07:04 +0800

    Update qndxx_script.py

--- qndxx_script.py
+++ qndxx_script.py
@@ -9,16 +9,19 @@ def run(openid):
         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x18002230) NetType/4G Language/zh_CNF'}
     payload = {'openid': openid}
     # get user info
+    print('get user info')
     url = 'http://qndxx.youth54.cn/SmartLA/dxxjfgl.w?method=getUserBasicInfo'
     res_user_info = requests.post(url=url, headers=headers, params=payload)
     user_info = res_user_info.json()
     print(user_info)
     # get current version
+    print('get current version')
     url = 'http://qndxx.youth54.cn/SmartLA/dxxjfgl.w?method=getNewestVersionInfo'
     res_get_version = requests.get(url=url, headers=headers)
     version = res_get_version.json()['version']
     print('version =', version)
     # learn qndxx
+    print('auto learning')
     url = 'http://qndxx.youth54.cn/SmartLA/dxxjfgl.w?method=studyLatest'
     headers['content-type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
     payload['version'] = version
